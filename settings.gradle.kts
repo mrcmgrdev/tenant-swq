42152cbe58b34d28fb6c983c104d82e0f6d1e2f5
mrcmgrdev <2410859018@hochschule-burgenland.at>
2026-03-20 18:43:06 +0100

    chore: add conventional commit commit-msg hook

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 plugins { id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.1.10" }
 
 gitHooks {
+  commitMsg { conventionalCommits() }
   preCommit { tasks("spotlessCheck") }
   createHooks()
 }
